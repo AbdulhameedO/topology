2943f817b227e1d75ed4745aba532d4e2e0cd108
abdulhameed osama <meedosama2001@gmail.com>
2022-05-31 04:15:00 +0200

    Add project files.

---
+++ topology/Source.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::vector;
+
+struct men
+{
+public:
+	int p1 = -1;
+	int p2 = -1;
+};
+
+int main()
+{
+	int count;
+	cin >> count;
+	int par1, par2;
+	vector<men> nas(count);
+
+	for (int i = 0; i < count; i++)
+	{
+		cin >> par1;
+		cin >> par2;
+
+		if (par1 == -1 && par2 == -1)
+			continue;
+		else if (par1 == -1)
+			nas[i].p2 = par2;
+		else if (par2 == -1)
+			nas[i].p2 = par1;
+		else if (par1 < par2)
+		{
+			nas[i].p1 = par1;
+			nas[i].p2 = par2;
+		}
+		else
+		{
+			nas[i].p2 = par1;
+			nas[i].p1 = par2;
+		}
+	}
+	return 0;
+}
